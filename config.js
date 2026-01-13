78a1fa463d3965d2c2033e521acbfd568ecaea58
hellostacia <tihonckova25@gmail.com>
2026-01-13 20:31:48 +0300

    добавлены адреса и ключи для api

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  API_BASE_URL: "http://exam-api-courses.std-900.ist.mospolytech.ru",
+  API_KEY: "559dad3a-0528-4244-9043-f92588eabf98",
+  PAGE_SIZE: 5,
+  YANDEX_MAPS_KEY: "8aff8709-5c08-4716-a78a-b3d897510701",
+};
